389b4d8064fa81e8b97c20d34218e8c7299072f8
Lorenzo Fontana <fontanalorenz@gmail.com>
2024-07-19 09:28:30 +0200

    new(pkg/observability): add logger

---
+++ pkg/observability/logger/logger.go
@@ -0,0 +1,46 @@
+package logger
+
+import (
+	"context"
+	"io"
+	"log/slog"
+)
+
+type (
+	loggerKey struct{}
+)
+
+func newProduction(w io.Writer) *slog.Logger {
+	level := slog.LevelInfo
+	handler := slog.NewJSONHandler(w, &slog.HandlerOptions{
+		Level: level,
+	})
+	return slog.New(handler)
+}
+
+func newDevelopment(w io.Writer) *slog.Logger {
+	level := slog.LevelDebug
+	handler := slog.NewTextHandler(w, &slog.HandlerOptions{
+		Level: level,
+	})
+	return slog.New(handler)
+}
+
+func New(w io.Writer, production bool) *slog.Logger {
+	if production {
+		return newProduction(w)
+	}
+	return newDevelopment(w)
+}
+
+func Context(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey{}, logger)
+}
+
+func FromContext(ctx context.Context) *slog.Logger {
+	logger := ctx.Value(loggerKey{})
+	if logger == nil {
+		return nil
+	}
+	return logger.(*slog.Logger)
+}
