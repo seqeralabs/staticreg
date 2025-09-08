df5c4ea4710b6453ade42fa1dbe6fc9716db0968
Lorenzo Fontana <fontanalorenz@gmail.com>
2025-09-08 17:22:16 +0200

    feat: support changing to dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["pkg/templates/tmpl/*.html", "pkg/templates/tmpl/partials/*.html", "pkg/templates/tmpl/partials/icons/*.html"],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
